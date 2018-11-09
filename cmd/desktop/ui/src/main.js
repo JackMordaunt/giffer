04d812ca98e315ecadefebe5b85d1ccf8afb1e29
Jack Mordaunt <jackmordaunt@gmail.com>
2018-11-09 17:33:54 +1300

    [+] Introduce semantic UI.

--- cmd/desktop/ui/src/main.js
+++ cmd/desktop/ui/src/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue"
 import App from "./App.vue"
+import SuiVue from "semantic-ui-vue"
+import "semantic-ui-css/semantic.min.css"
 
+Vue.use(SuiVue)
 Vue.config.productionTip = false
 
 new Vue({
