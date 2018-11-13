af99b56e3b0e6d222e862633a8b83c5073fe2876
Jack Mordaunt <jackmordaunt@gmail.com>
2018-11-14 11:40:59 +1300

    [+] Import normalize.css.

--- cmd/desktop/ui/src/main.js
+++ cmd/desktop/ui/src/main.js
@@ -2,10 +2,11 @@ import Vue from "vue"
 import App from "./App.vue"
 import SuiVue from "semantic-ui-vue"
 import "semantic-ui-css/semantic.min.css"
+import "normalize.css"
 
 Vue.use(SuiVue)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
