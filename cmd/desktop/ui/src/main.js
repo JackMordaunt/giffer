f6fea8c3388ef8d60797bbff6d5b556000ceb0fc
Jack Mordaunt <jackmordaunt@gmail.com>
2018-11-28 15:24:58 -0800

    [+] Use babel polyfill for ie11.

--- cmd/desktop/ui/src/main.js
+++ cmd/desktop/ui/src/main.js
@@ -1,8 +1,10 @@
+import "@babel/polyfill"
+import "semantic-ui-css/semantic.min.css"
+import "normalize.css"
+
 import Vue from "vue"
 import App from "./App.vue"
 import SuiVue from "semantic-ui-vue"
-import "semantic-ui-css/semantic.min.css"
-import "normalize.css"
 
 Vue.use(SuiVue)
 Vue.config.productionTip = false
