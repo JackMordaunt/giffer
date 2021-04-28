7788cbe96491abe0db1fc465cc5cd667cd71880c
Jack Mordaunt <jackmordaunt@gmail.com>
2021-04-28 20:02:40 +0800

    refactor: use kddai/youtube as downloader

--- download.go
+++ download.go
@@ -1,67 +1,42 @@
 package giffer
 
 import (
-	"io"
-	"github.com/OneOfOne/xxhash"
+	"context"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"strings"
-	"os"
-	"github.com/jackmordaunt/video-downloader/config"
-	"github.com/pkg/errors"
-	"net/url"
 
-	"github.com/jackmordaunt/video-downloader/downloader"
-	"github.com/jackmordaunt/video-downloader/extractors/bcy"
-	"github.com/jackmordaunt/video-downloader/extractors/bilibili"
-	"github.com/jackmordaunt/video-downloader/extractors/douyin"
-	"github.com/jackmordaunt/video-downloader/extractors/douyu"
-	"github.com/jackmordaunt/video-downloader/extractors/facebook"
-	"github.com/jackmordaunt/video-downloader/extractors/instagram"
-	"github.com/jackmordaunt/video-downloader/extractors/iqiyi"
-	"github.com/jackmordaunt/video-downloader/extractors/mgtv"
-	"github.com/jackmordaunt/video-downloader/extractors/miaopai"
-	"github.com/jackmordaunt/video-downloader/extractors/pixivision"
-	"github.com/jackmordaunt/video-downloader/extractors/qq"
-	"github.com/jackmordaunt/video-downloader/extractors/tumblr"
-	"github.com/jackmordaunt/video-downloader/extractors/twitter"
-	"github.com/jackmordaunt/video-downloader/extractors/universal"
-	"github.com/jackmordaunt/video-downloader/extractors/vimeo"
-	"github.com/jackmordaunt/video-downloader/extractors/weibo"
-	"github.com/jackmordaunt/video-downloader/extractors/youku"
-	"github.com/jackmordaunt/video-downloader/extractors/youtube"
-	"github.com/jackmordaunt/video-downloader/utils"
+	"github.com/OneOfOne/xxhash"
+	"github.com/kkdai/youtube/v2"
+	"github.com/kkdai/youtube/v2/downloader"
+	"github.com/pkg/errors"
 )
 
 // Downloader is responsible for downloading videos.
 type Downloader struct {
-	Dir string
+	Dir    string
 	FFmpeg string
-	Debug bool
-	Out io.Writer
+	Debug  bool
+	Out    io.Writer
 }
 
-// Download the video from URL into Dir and return the full path to the 
+// Download the video from URL into Dir and return the full path to the
 // downloaded file.
 func (dl Downloader) Download(
 	URL string,
 	start, end float64,
 	q Quality,
 ) (string, error) {
-	// Side channel for loading config because of how the package is
-	// unfortunately structured.
-	config.FFmpeg = dl.FFmpeg
-	dl.logf("ffmpeg: %q\n", dl.FFmpeg)	
+	dl.logf("ffmpeg: %q\n", dl.FFmpeg)
 	input := fmt.Sprintf("%s_%f_%f_%s", URL, start, end, q)
 	h, err := hash(input)
 	if err != nil {
 		return "", errors.Wrap(err, "creating hash")
 	}
 	dir := filepath.Join(dl.Dir, h)
-	// Side channel for loading config because of how the package is
-	// unfortunately structured.
-	config.OutputPath = dir
 	// Return cached file if it exists.
 	entries, _ := ioutil.ReadDir(dir)
 	for _, entry := range entries {
@@ -75,6 +50,7 @@ func (dl Downloader) Download(
 	if err := os.MkdirAll(dir, 0755); err != nil && !os.IsExist(err) {
 		return "", errors.Wrap(err, "preparing directories")
 	}
+
 	tmp, err := dl.download(URL, start, end, q)
 	if err != nil {
 		return "", err
@@ -88,13 +64,13 @@ func (dl Downloader) Download(
 
 func (dl Downloader) logf(f string, v ...interface{}) {
 	if !dl.Debug || dl.Out == nil {
-		return 
+		return
 	}
 	fmt.Fprintf(dl.Out, f, v...)
 }
 
 // Quality is an enum representing the various video qualities.
-type Quality int 
+type Quality int
 
 // Matches returns true if the input represents the quality as a string.
 // This is primarily an adaptor so we don't have to change the video-downloader
@@ -102,13 +78,13 @@ type Quality int
 func (q Quality) Matches(str string) bool {
 	var patterns []string
 	switch q {
-	case Best: 
+	case Best:
 		patterns = append(patterns, "1080p")
-	case High: 
+	case High:
 		patterns = append(patterns, "720p", "480p")
-	case Medium: 
+	case Medium:
 		patterns = append(patterns, "360p")
-	case Low: 
+	case Low:
 		patterns = append(patterns, "240p", "144p")
 	}
 	for _, p := range patterns {
@@ -123,7 +99,7 @@ const (
 	// Low 144p
 	Low Quality = iota
 	// Medium 360p
-	Medium 
+	Medium
 	// High 720p
 	High
 	// Best 1080p@60 > 720p@60 > 1080p > 720p
@@ -132,15 +108,15 @@ const (
 
 func (q Quality) String() string {
 	switch q {
-	case Low: 
+	case Low:
 		return "low"
 	case Medium:
 		return "medium"
-	case High: 
+	case High:
 		return "high"
 	case Best:
 		return "best"
-	default: 
+	default:
 		return "unknown"
 	}
 }
@@ -151,90 +127,15 @@ func (dl Downloader) download(
 	start, end float64,
 	quality Quality,
 ) (string, error) {
-	var (
-		domain string
-		err    error
-		data   []downloader.Data
-	)
-	bilibiliShortLink := utils.MatchOneOf(videoURL, `^(av|ep)\d+`)
-	if bilibiliShortLink != nil {
-		bilibiliURL := map[string]string{
-			"av": "https://www.bilibili.com/video/",
-			"ep": "https://www.bilibili.com/bangumi/play/",
-		}
-		domain = "bilibili"
-		videoURL = bilibiliURL[bilibiliShortLink[1]] + videoURL
-	} else {
-		u, err := url.ParseRequestURI(videoURL)
-		if err != nil {
-			return "", errors.Wrap(err, "parsing uri")
-		}
-		domain = utils.Domain(u.Host)
+	d := downloader.Downloader{
+		Client: youtube.Client{},
 	}
-	switch domain {
-	case "douyin", "iesdouyin":
-		data, err = douyin.Download(videoURL)
-	case "bilibili":
-		data, err = bilibili.Download(videoURL)
-	case "bcy":
-		data, err = bcy.Download(videoURL)
-	case "pixivision":
-		data, err = pixivision.Download(videoURL)
-	case "youku":
-		data, err = youku.Download(videoURL)
-	case "youtube", "youtu": // youtu.be
-		data, err = youtube.Download(videoURL)
-	case "iqiyi":
-		data, err = iqiyi.Download(videoURL)
-	case "mgtv":
-		data, err = mgtv.Download(videoURL)
-	case "tumblr":
-		data, err = tumblr.Download(videoURL)
-	case "vimeo":
-		data, err = vimeo.Download(videoURL)
-	case "facebook":
-		data, err = facebook.Download(videoURL)
-	case "douyu":
-		data, err = douyu.Download(videoURL)
-	case "miaopai":
-		data, err = miaopai.Download(videoURL)
-	case "weibo":
-		data, err = weibo.Download(videoURL)
-	case "instagram":
-		data, err = instagram.Download(videoURL)
-	case "twitter":
-		data, err = twitter.Download(videoURL)
-	case "qq":
-		data, err = qq.Download(videoURL)
-	default:
-		data, err = universal.Download(videoURL)
-	}
+	v, err := d.GetVideo(videoURL)
 	if err != nil {
-		return "", errors.Wrap(err, "preparing")
+		return "", fmt.Errorf("getting video: %w", err)
 	}
-	var path string
-	for _, item := range data {
-		if item.Err != nil {
-			return "", errors.Wrap(item.Err, "extracting")
-		}
-		for k, stream := range item.Streams {
-			if quality.Matches(stream.Quality) {
-				config.Stream = k
-				break
-			}
-		}
-		cfg := config.NewFromGlobal()
-		cfg.FFmpeg = dl.FFmpeg
-		d := downloader.Downloader{
-			Config: cfg,
-			Output: dl.Out,
-		}
-		path, err = d.Download(&item, videoURL, start, end)
-		if err != nil {
-			return "", err
-		}
-	}
-	return path, nil
+	outf := filepath.Join(os.TempDir(), v.Title)
+	return outf, d.Download(context.TODO(), v, &v.Formats[0], outf)
 }
 
 func hash(input string) (string, error) {
--- go.mod
+++ go.mod
@@ -1,23 +1,25 @@
 module github.com/jackmordaunt/giffer
 
+go 1.16
+
 require (
 	github.com/GeertJohan/go.rice v0.0.0-20170420135705-c02ca9a983da
 	github.com/OneOfOne/xxhash v1.2.2
-	github.com/PuerkitoBio/goquery v1.5.0 // indirect
-	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
 	github.com/daaku/go.zipexe v0.0.0-20150329023125-a5fe2436ffcb // indirect
 	github.com/gonutz/ide v0.0.0-20180502124734-e9fc8c14ed56
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.6.2
-	github.com/gorilla/websocket v1.4.0
+	github.com/gorilla/websocket v1.4.2
 	github.com/hashicorp/go-multierror v1.0.0
 	github.com/jackmordaunt/icns v1.0.0
-	github.com/jackmordaunt/video-downloader v0.0.0-20181119094506-2a87091c9920
 	github.com/kardianos/osext v0.0.0-20170510131534-ae77be60afb1 // indirect
+	github.com/kkdai/youtube/v2 v2.6.1
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
-	github.com/pkg/errors v0.8.0
+	github.com/pkg/errors v0.8.1
 	github.com/zserge/lorca v0.1.2
 	github.com/zserge/webview v0.0.0-20181018084947-f390a2df9ec5
-	golang.org/x/crypto v0.0.0-20181126163421-e657309f52e7
-	golang.org/x/sys v0.0.0-20181116161606-93218def8b18 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+	golang.org/x/net v0.0.0-20210427231257-85d9c07bbe3a // indirect
+	golang.org/x/sys v0.0.0-20210426230700-d19ff857e887 // indirect
 )
